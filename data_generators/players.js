15aec4e7ff9077a25b983b01e1f2a273c0ed2cab
Cody Rose <codyjrose@gmail.com>
2014-11-16 18:54:20 -0500

    Added players file to generate fake players data

--- data_generators/players.js
+++ data_generators/players.js
@@ -1,3 +1,17 @@
-/**
- * Created by cody on 11/15/14.
- */
+// Run from shell: mongo localhost:27017/bizfoosTestDb data_generators/players.js
+
+// Clear out current players
+db.players.remove({});
+
+// Load with defaults
+db.players.insert({ name : "Marky Mark" });
+db.players.insert({ name : "Mcbeev" });
+db.players.insert({ name : "Batman" });
+db.players.insert({ name : "CVB" });
+db.players.insert({ name : "Adam" });
+db.players.insert({ name : "Al" });
+db.players.insert({ name : "Dustin " });
+db.players.insert({ name : "Kevin" });
+db.players.insert({ name : "Blair" });
+db.players.insert({ name : "Dan" });
+db.players.insert({ name : "Cody" });
