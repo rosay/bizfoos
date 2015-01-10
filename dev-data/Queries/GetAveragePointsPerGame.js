0583e6491f5c8a6e52e5b46162bcd0f08bc82d7a
Mark Schmidt <mschmidt@bizstream.com>
2015-01-09 21:24:33 -0500

    Added New fields to see Offensive vs Defensive points as well

--- dev-data/Queries/GetAveragePointsPerGame.js
+++ dev-data/Queries/GetAveragePointsPerGame.js
@@ -3,12 +3,26 @@ var PlayerPoints = db.games.aggregate([
     ,{ $unwind: "$scores" }
     ,{ $group: { 
         _id: "$roster.player_id" ,
-        TotalPoints: { $sum: { $cond: { if: { $eq: ["$roster.player_id", "$scores.player_id"] }, then: 1, else: 0 } } },
-        } 
+        TotalPointsOnOffense: { $sum: { $cond: { if: 
+            { "$and": [
+                { $eq: ["$roster.player_id", "$scores.player_id"] }
+                ,{ $eq: ["$roster.position", "offense"] }
+            ]}
+            , then: 1, else: 0 } } 
+        },
+        TotalPointsOnDefense: { $sum: { $cond: { if: 
+            { "$and": [
+                { $eq: ["$roster.player_id", "$scores.player_id"] }
+                ,{ $eq: ["$roster.position", "defense"] }
+            ]}
+            , then: 1, else: 0 } } 
+        },
      }
+ }
     ,{ $project: {
         Player: "$roster.player_id",
-        TotalPoints: "$TotalPoints"
+        TotalPointsOnOffense: "$TotalPointsOnOffense",
+        TotalPointsOnDefense: "$TotalPointsOnDefense"
     }
 }
 ]);
@@ -31,8 +45,13 @@ PlayerGames.result.forEach(function(item) {
     var playerPointData = PlayerPoints.result.filter(function(entry) {
         return (entry._id == item._id)
     });
-    item.TotalPoints = playerPointData[0].TotalPoints;
-    item.AveragePointsPerGame = item.TotalPoints / item.TotalGames;
+    item.TotalOffensivePoints = playerPointData[0].TotalPointsOnOffense;
+    item.TotalDefensivePoints = playerPointData[0].TotalPointsOnDefense;
+    item.TotalPoints = item.TotalOffensivePoints + item.TotalDefensivePoints;
+
+    item.AverageOffensivePointsPerGame = item.TotalOffensivePoints / item.TotalGames;
+    item.AverageDefensivePointsPerGame = item.TotalDefensivePoints / item.TotalGames;
+    item.AveragePointsPerGame          = item.TotalPoints          / item.TotalGames;
 });
 
 //sort it
