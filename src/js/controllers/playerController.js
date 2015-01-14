da6c95602df334a5e9fe69f1a058be82e71a44f5
Nick Beukema <nick.beukema@gmail.com>
2015-01-14 14:45:03 -0500

    cleaned up code

--- src/js/controllers/playerController.js
+++ src/js/controllers/playerController.js
@@ -30,19 +30,5 @@ app.controller('PlayerController', ['gameService', 'playerService', function (ga
 		playerService.removePlayerFromBullpen(playerId);
 		vm.bullpenCount = playerService.getBullpenCount();
 	};
-}])
-.directive('foosEnter', function(){
-	return function(scope, element, attrs){
-		element.bind("keydown keypress", function(event){
-			if(event.which === 13){
-				scope.$apply(function(){
-					scope.ctrl.addPlayerToBullpen(scope.filteredPlayers[0]._id);
-					scope.searchPlayers = '';
-				});
-
-				event.preventDefault();
-			}
-		});
-	};
-});
+}]);
 
